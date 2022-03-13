f6234a0504bdae79633a3615244bd5e0941c956a
Kalindu Sekarage <69759530+ksekara@users.noreply.github.com>
2022-03-13 08:42:12 +0530

    Update AckleyFunction.java

--- AckleyFunction.java
+++ AckleyFunction.java
@@ -1,8 +1,12 @@
+import com.opencsv.CSVWriter;
 import io.jenetics.*;
 import io.jenetics.engine.Engine;
 import io.jenetics.engine.EvolutionStatistics;
 import io.jenetics.stat.DoubleMomentStatistics;
 import io.jenetics.util.Factory;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.Math;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +15,13 @@ import static io.jenetics.engine.Limits.*;
 import static java.lang.Math.cos;
 import static java.lang.Math.PI;
 
+/**
+ * @author 218541J D.K Sekarage
+ * @author 218552T S.U Yapa
+ **/
+
 public class AckleyFunction {
-    static final int CASE_NO = 2;
+    static final int CASE_NO = 3;
 
     // Definition of the fitness function.e
     private static double fitness(Genotype<DoubleGene> gt) {
@@ -24,7 +33,7 @@ public class AckleyFunction {
 
         //fitness function
         fitness = -20.0*Math.exp(-0.2*Math.sqrt(0.5 * ((X1*X1)+(X2*X2))))-Math.exp(0.5*(cos(2*PI*(double)X1)+cos(2*PI*(double)X2)))+Math.exp(1.0)+20;
-        //System.out.println(fitness);
+
         return fitness;
     }
     private static Engine<DoubleGene, Double> selectedCase(Engine.Builder<DoubleGene, Double> builder){
@@ -51,60 +60,79 @@ public class AckleyFunction {
                     .survivorsSelector(new TournamentSelector<>())
                     .build();
 
+            case 3-> builder
+                    .populationSize(200)
+                    .selector(new StochasticUniversalSelector<>())
+                    .optimize(Optimize.MINIMUM)
+                    .alterers(
+                            new Mutator<>(0.5),
+                            new MultiPointCrossover<>(0.9)
+                    )
+                    .survivorsSelector(new TruncationSelector<>())
+                    .build();
+
             default -> throw new IllegalStateException("Enter a Correct Case No");
         };
     }
 
 
     public static void main(String[] args) {
-        // 1.) Define the genotype (factory) suitable for the problem.
+        // Define the genotype (factory) suitable for the problem.
         Factory<Genotype<DoubleGene>> gtfDouble = Genotype.of(DoubleChromosome.of(-5, 5, 2));
 
-        List<String[]> bestPhenotypeForEveryGeneration = new ArrayList<>();
+        List<String[]> recordForeveryGen = new ArrayList<>();
         // engine creation
         Engine.Builder<DoubleGene, Double> builder = Engine.builder(AckleyFunction::fitness, gtfDouble);
         Engine<DoubleGene, Double> engine = selectedCase(builder);
 
-        // 4.) Start the execution (evolution) and collect the result.
+        // Start the execution (evolution) and collect the result.
         final EvolutionStatistics<Double, DoubleMomentStatistics> statisticsDouble = EvolutionStatistics.ofNumber();
 
         engine.stream()
         //.limit(bySteadyFitness(50))
-        .limit(byFitnessConvergence(10,30, 10E-4))
-        //.limit(200)
+        //.limit(byFitnessConvergence(10,30, 10E-4))
+        .limit(200)
         .peek(statisticsDouble)
         .forEach(
-                //result -> System.out.println(result.bestPhenotype())
-                result -> {
-//                    bestPhenotypeForEveryGeneration.add(result.bestPhenotype());
-                    List<String> generationOutput = new ArrayList<>();
+                  result -> {
+                    List<String> outRecord = new ArrayList<>();
 
                     String generation = String.valueOf(result.generation());
-                    String bestGenotype = result.bestPhenotype().genotype().chromosome().toString();
-                    String worstGenotype = result.worstPhenotype().genotype().chromosome().toString();
-                    String worstFitness = result.worstFitness().toString();
-                    String bestFitness = result.bestPhenotype().fitness().toString();
-                    String averageFitness = String.valueOf(
+                    String bestChromosome = result.bestPhenotype().genotype().chromosome().toString();
+                    String bestFitnessVal = result.bestPhenotype().fitness().toString();
+                    String avgFitness = String.valueOf(
                             result.population().stream().mapToDouble(Phenotype::fitness).average().orElseThrow()
                     );
 
-                    generationOutput.add(generation);
-                    generationOutput.add(worstGenotype);
-                    generationOutput.add(worstFitness);
-                    generationOutput.add(bestGenotype);
-                    generationOutput.add(bestFitness);
-                    generationOutput.add(averageFitness);
+                      outRecord.add(generation);
+                      outRecord.add(bestChromosome);
+                      outRecord.add(bestFitnessVal);
+                      outRecord.add(avgFitness);
 
-                    String[] row = new String[generationOutput.size()];
-                    generationOutput.toArray(row);
+                    String[] row = new String[outRecord.size()];
+                      outRecord.toArray(row);
 
-                    bestPhenotypeForEveryGeneration.add(row);
+                    recordForeveryGen.add(row);
                 });
 
 
-       
+        AckleyFunction.createCSV(recordForeveryGen, "test-limit.csv");
         System.out.println("Results :\n" + statisticsDouble);
+
 
     }
 
+    public static void createCSV(List<String[]> data, String filePath) {
+
+        try {
+            CSVWriter writer = new CSVWriter(new FileWriter(filePath));
+            for (String[] row : data) {
+                writer.writeNext(row);
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
